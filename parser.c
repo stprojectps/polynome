3f040e41b338121ce9ebcde2e8173b24be8a92b4
Pape Moussa SARR <pbodiansarr@gmail.com>
2019-11-09 03:15:16 +0100

    2019-11-09 - [main_1] Function for checking if a component is valid or not
      [NEW] int is_valid_component(const char*, int, int) => [parser.*]: function that checks if a component is valid or not and returns respectively 1 or 0
      [UPDATE] road_map.txt

--- parser.c
+++ parser.c
@@ -14,3 +14,39 @@ int is_valid_char(const char _c)
 
     return 0;
 }
+
+int is_valid_component(const char* _raw_poly, int _start_component, int _end_component)
+{
+    int
+    sign = 0,
+    digits = 0,
+    x = 0;
+    
+    int i;
+    for(i = _start_component; i <= _end_component || *(_raw_poly+i); i++)
+    {
+        if(sign || digits || x)
+        {
+            if(*(_raw_poly+i) == '+' || *(_raw_poly+i) == '-')
+            {
+                return 0;
+            }
+        }
+
+        if(x)
+        {
+            if(*(_raw_poly+i) == 'x')
+            {
+                return 0;
+            }
+        }
+
+        if(*(_raw_poly+i) == 'x') { x = 1; }
+        
+        if(*(_raw_poly+i) == '+' || *(_raw_poly+i) == '-') { sign = 1; }
+
+        if(*(_raw_poly+i) >= '0' && *(_raw_poly+i) <= '9') { digits = 1; }
+    }
+
+    return 1;
+}
--- parser.h
+++ parser.h
@@ -1,9 +1,25 @@
+/**
+ * THIS IS A POLYNOME PARSER PROJECT
+ * THE PARSER CAN PARSE SOME POLYNOMES
+ * RESPECTING THE FOLLOWING RULES:
+ * 
+ *  - MONOMES WHICH CONSTITUTE THE
+ *    POLYNOME MUST APPEAR IN DECREASING
+ *    ORDER STARTING BY THE LEFT
+ *  - THE POLYNOME MUST BE A POLYNOME WITH
+ *    ONE VARIABLE
+ * 
+ * 
+ */
+
 #ifndef H_PARSER_H
 #define H_PARSER_H
 
+#include "./poly.h"
+
 #define LENGHT_EXPRESSIONS 13
 
-char EXPRESSIONS[LENGHT_EXPRESSIONS] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', ' '};
+static const char EXPRESSIONS[LENGHT_EXPRESSIONS] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', ' '};
 
 /**
  * Checks if the given parameter is 
@@ -14,4 +30,15 @@ char EXPRESSIONS[LENGHT_EXPRESSIONS] = {'0', '1', '2', '3', '4', '5', '6', '7', 
  * @return {int}        - 1 if valid char, 0 otherwise
  */
 int is_valid_char(const char _c);
+
+/**
+ * Checks if a component is valid.
+ * 
+ * @param  {const char*}    - raw polynome
+ * @param  {int}            - start position of component
+ * @param  {int}            - end position of component
+ * @return {int}            - 1 if valid component, 0 otherwise
+ */
+int is_valid_component(const char* _raw_poly, int _start_component, int _end_component);
+
 #endif //H_PARSER_H
