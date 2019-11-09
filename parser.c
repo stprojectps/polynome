a11dddac76d618387e1b10a2204c625e71ba5470
GreenTAM <stambadou@groupeisi.com>
2019-11-09 08:02:37 +0000

    2019-11-09-[main-2]Function for extracting sign/number/power of a component
    	[NEW]	-int extract_sign(const char*, int) => [parser.*]:function extracting the sign of a component and return 1 if positive, -1 otherwise
    		-int extract_number(const char*, int, int) => [parser.*]:function extracting the number of a component and return it
    		-int extract_power(const char*, int, int) => [parser.*]:function extracting the power of a component and return it if the component is a monome,0 otherwise
    	[UPDATE] road_map.txt, parser.*

--- parser.c
+++ parser.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "parser.h"
 
 int is_valid_char(const char _c)
@@ -49,4 +50,65 @@ int is_valid_component(const char* _raw_poly, int _start_component, int _end_com
     }
 
     return 1;
+}
+
+int extract_sign(const char *_raw_poly, int _start_component)
+{
+    if(*(_raw_poly+_start_component) == '-')
+    {
+        return -1;
+    }
+    
+    return 1;
+}
+
+int extract_number(const char *_raw_poly, int _start_component, int _end_component)
+{
+    int number = 0;
+    int i;
+
+    for(i = _start_component; i <= _end_component; i++)
+    {
+        if(*(_raw_poly+i) == ' ' || *(_raw_poly+i) == '+' || *(_raw_poly+i) == '-')
+        {
+            continue;
+        }
+
+        if(*(_raw_poly+i) == 'x')
+        {
+            if(number == 0)
+            {
+                number++;
+            }
+
+            break;
+        }
+        
+        number = (number*10)+(*(_raw_poly+i)-'0');
+    }
+    
+    return number;
+}
+
+int extract_power(const char *_raw_poly, int _start_component, int _end_component)
+{
+    int power = 1;
+    int i;
+    int j;
+
+    for(i = _start_component; i <= _end_component; i++)
+    {
+        if(*(_raw_poly+i) == 'x')
+        {    
+            for(j = i+1; j <= _end_component; j++)
+            {
+                printf("\ni=%d => %d => power = %d\n", i, j, power);
+                power = (power*10)+(*(_raw_poly+j)-'0');
+            }
+
+            return power;
+        }      
+    }
+    
+    return 0;
 }
--- parser.h
+++ parser.h
@@ -41,4 +41,44 @@ int is_valid_char(const char _c);
  */
 int is_valid_component(const char* _raw_poly, int _start_component, int _end_component);
 
+/**
+* Function for extracting component sign
+* @hint: just search in the left side for sign. When the sign('+'/'-')
+* is detected return the good value. Sometimes sign is omitted,
+* so we need to break and return the good value at the first
+* digit or 'x'.
+*
+* @param  {const char*}	- polynome
+* @param  {int} 		- start position of component
+* @param  {int} 		- end position of component
+* @return {int}			- 1 if positive, -1 if negative
+*/
+int extract_sign(const char *_raw_poly, int _start_component);
+
+/**
+* Function for extracting number
+* @hint: Sign MUST NOT be considered, only the coefficient
+* 		 is important. we need to consider the first digit
+*        starting by the left after the sign and stop when
+*		 'x' or end position is reached. The conversion MUST
+*		 be done.
+*
+* @param  {const char*}	- polynome
+* @param  {int}			- start position of component
+* @param  {int}			- end position of component
+* @return {int}			- extracted number
+*/
+int extract_number(const char *_raw_poly, int _start_component, int _end_component);
+
+/**
+* Function for extraction power
+* @hint: Extact the power of an component if this one is  a monome
+*
+* @param  {const char*}	- polynome
+* @param  {int}			- start position of component
+* @param  {int}			- end position of component
+* @return {int}			- extracted power, 0 otherwise
+*/
+int extract_power(const char *_raw_poly, int _start_component, int _end_component);
+
 #endif //H_PARSER_H
